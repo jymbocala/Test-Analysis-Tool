3c89f8a55633a43e2afe93af43a3a0a12a513516
Jym Bocala <jym.bocala@gmail.com>
2022-09-09 23:44:38 +1000

    add service workers and css minimizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -8,6 +12,9 @@ module.exports = {
   output: {
     libraryTarget: "var",
     library: "Client",
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})],
   },
   module: {
     rules: [
@@ -18,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -27,5 +34,9 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
