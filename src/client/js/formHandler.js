630ed05343dfad7a6fe172a47e49e1450bd2092f
Jym Bocala <jym.bocala@gmail.com>
2022-09-19 22:16:08 +1000

    hide api key

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,44 +1,86 @@
-let articleURL = "";
-
 function handleSubmit(event) {
   event.preventDefault();
+  
+  const resultsEl = document.getElementById("results-el");
+  const articleURL = document.getElementById("article-url").value;
+  
+  console.log("Analyze button clicked");
 
-  // check what text was put into the form field
-  let formText = document.getElementById("name").value;
+  resultsEl.innerHTML = `
+        <h2> Results </h2>
+        <div class="loader"></div> 
+        `; 
 
-  Client.checkForName(formText);
-
-  console.log("::: Form Submitted :::");
-  fetch("http://localhost:8081/test")
-    .then((res) => res.json())
-    .then(function (data) {
-      document.getElementById("results").innerHTML = data.message;
-    });
-
-  // get URL for API
-  articleURL = document.getElementById("article-url").value;
-
-  // MeaningCloud API
-  const formdata = new FormData();
-  formdata.append("key", '21dba14f5feadfb31d6cd69def6054ac');
-  formdata.append("url", articleURL); // GET URL FROM INPUT
-  formdata.append("lang", "en");
-
+function noResult() {
+  resultsEl.innerHTML = `
+        <h2> Results </h2>`
+}
   const requestOptions = {
     method: "POST",
-    body: formdata,
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify({url: articleURL}),
     redirect: "follow",
   };
 
   const response = fetch(
-    "https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-    .then((response) => ({
-      status: response.status,
-      body: response.json(),
-    }))
-    .then(({ status, body }) => console.log(status, body))
+    '/analyze',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      if (data.status.msg === "Operation denied") {
+        noResult();
+        alert("Please enter a valid URL.");
+        return
+      } else if (data.status.msg === "missing required parameter(s): please specify one of txt, url, doc") {
+        noResult();
+        alert("Input field cannot be empty. Please enter a URL.");
+        return
+      }
+
+      let polarity = data.score_tag;
+      const confidence = data.confidence;
+      const agreement = data.agreement;
+      const subjectivity = data.subjectivity;
+      let irony = data.irony;
+
+      if (polarity === "P+") {
+        polarity = "strongly positive";
+      } else if (polarity === "P") {
+        polarity = "positive";
+      } else if (polarity === "NEU") {
+        polarity = "neutral";
+      } else if (polarity === "N") {
+        polarity = "negative";
+      } else if (polarity === "N+") {
+        polarity = "strongly negative";
+      } else {
+        polarity = "without polarity";
+      }
+
+      if (irony === "NONIRONIC") {
+        irony = "without irony";
+      } else if (irony === "IRONIC") {
+        irony = "ironic";
+      }
+
+
+      // do DOM manipulation here!
+      function renderResults() {
+        resultsEl.innerHTML = `
+        <h2> Results </h2> <br> <p>This text is <span class="results-text">${polarity}</span> with a confidence of a <span class="results-text">${confidence}</span> percent. The polarities detected in it are in <span class="results-text">${agreement}</span>. The text is <span class="results-text">${subjectivity}</span> and <span class="results-text">${irony}</span>.</p>
+        `;
+      }
+
+      renderResults();
+    })
     .catch((error) => console.log("error", error));
+
 }
 
 export { handleSubmit };
-
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const FormData = require('form-data');
+
+const fetch = require('node-fetch');
+
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
@@ -8,6 +12,7 @@ const mockAPIResponse = require("./mockAPI.js");
 const app = express();
 
 app.use(express.static("dist"));
+app.use(express.json()) // for parsing application/json
 
 console.log(__dirname);
 
@@ -24,31 +29,32 @@ app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
 });
 
-// import import { handleSubmit } from "./js/formHandler";
-import { articleURL } from "./js/formHandler";
-
 // API credentials
-const mcapi = new meaningcloud({
-  application_key: process.env.API_KEY
+const api_key = process.env.API_KEY
+
+app.post("/analyze", function (req, res) {
+  const formdata = new FormData();
+  formdata.append("key", api_key);
+  formdata.append("url", req.body.url);
+  formdata.append("lang", "en");
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+  console.log(req.body)
+  console.log(requestOptions);
+  fetch(
+    "https://api.meaningcloud.com/sentiment-2.1",
+    requestOptions
+  )
+    .then((response) => {
+      console.log(response)
+      return response.json()
+    })
+    .then((data) => {
+      res.send(data);
+    })
 });
 
-// API REQUEST
-// const formdata = new FormData();
-// formdata.append("key", process.env.API_KEY);
-// formdata.append("url", articleURL); // GET URL FROM INPUT
-// formdata.append("lang", "en");
-
-// const requestOptions = {
-//   method: "POST",
-//   body: formdata,
-//   redirect: "follow",
-// };
-
-// const response = fetch(
-//   "https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-//   .then((response) => ({
-//     status: response.status,
-//     body: response.json(),
-//   }))
-//   .then(({ status, body }) => console.log(status, body))
-//   .catch((error) => console.log("error", error));
